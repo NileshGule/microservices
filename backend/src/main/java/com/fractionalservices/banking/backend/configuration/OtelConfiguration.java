4bc67236d4cbdc730c1bfa6ddba7711a1e801f9b
info@fractionalservices.co <info@fractionalservices.co>
2023-07-28 16:00:43 +0800

    Modify otel configuration

--- backend/src/main/java/com/fractionalservices/banking/backend/configuration/OtelConfiguration.java
+++ backend/src/main/java/com/fractionalservices/banking/backend/configuration/OtelConfiguration.java
@@ -1,11 +1,9 @@
 package com.fractionalservices.banking.backend.configuration;
 
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
