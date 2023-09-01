557c2abd19047622245a4614185a69d6231fbf09
Nilesh Gule <vn_nilesh@yahoo.com>
2023-09-01 23:11:19 +0800

    working version of otel collector

--- k8s-AKS/scripts/helm-install.sh
+++ k8s-AKS/scripts/helm-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add jetstack https://charts.jetstack.io
 
@@ -21,6 +22,12 @@ helm upgrade --install prometheus \
 prometheus-community/kube-prometheus-stack  \
 --values prometheus/prometheus_values.yaml \
 --set web.enable-remote-write-receiver=true \
+--wait
+
+# Loki
+helm upgrade --install loki \
+grafana/loki-stack  \
+--set loki.isDefault=false
 --wait
 
 
